ea6953f133968e61c69a8661dd2750bb5e576934
Jonhatan <jonyvilla44@gmail.com>
2024-02-21 22:09:49 -0600

    Corrigiendo errores

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const reviews = [
       id: 1,
       name: 'susan smith',
       job: 'web developer',
-      img: 'https://www.course-api.com/images/people/person-1.jpeg',
+      img: 'https://www.course-api.com/images/people/person-5.jpeg',
       text: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
     },
     {
@@ -17,14 +17,14 @@ const reviews = [
       id: 3,
       name: 'peter jones',
       job: 'intern',
-      img: 'https://www.course-api.com/images/people/person-4.jpeg',
+      img: 'https://www.course-api.com/images/people/person-3.jpeg',
       text: 'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
     },
     {
       id: 4,
       name: 'bill anderson',
       job: 'the boss',
-      img: 'https://www.course-api.com/images/people/person-3.jpeg',
+      img: 'https://www.course-api.com/images/people/person-4.jpeg',
       text: 'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
     },
   ];
@@ -40,31 +40,36 @@ const button = document.querySelector('.random-btn');
 
 let currentPerson = 0;
 
+window.addEventListener('DOMContentLoaded', () => {
+    showInfo();
+});
+
 //SHOW PERSON
-const showInfo = (index) => {
-    if(reviews[index]) {
-        author.textContent = reviews[index].name;
-        job.textContent = reviews[index].job;
-        personImg.src = reviews[index].img;
-        info.textContent = reviews[index].text;
-    }
+const showInfo = () => {
+    const item = reviews[currentPerson];
+    author.textContent = item.name;
+    job.textContent = item.job;
+    personImg.src = item.img;
+    info.textContent = item.text;
 };
 //PREVIOUS PERSON
 prevBtn.addEventListener('click', () => {
-    if(currentPerson > 0) {
-        currentPerson--;
-        showInfo(currentPerson);
+    currentPerson--;
+    if(currentPerson < 0) {
+        currentPerson = reviews.length - 1;
     }
+    showInfo();
 });
 //NEXT PERSON
 nextBtn.addEventListener('click', () => {
-    if(currentPerson < reviews.length-1) {
-        currentPerson++;
-        showInfo(currentPerson);
-        
+    currentPerson++;
+    if(currentPerson > reviews.length - 1) {
+        currentPerson = 0;
     }
+    showInfo();
 });
 
 button.addEventListener('click', () => {
-    showInfo(Math.floor(Math.random() * reviews.length));
+    currentPerson = Math.floor(Math.random() * reviews.length);
+    showInfo();
 });
